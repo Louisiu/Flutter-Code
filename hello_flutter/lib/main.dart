155dfcc6689963ba7c373320a014aaacb102ec9a
fengshuo.liu <fsliu136@gmail.com>
2019-06-30 17:47:50 +0800

    -m add Widget

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,26 @@
+// dart 面向对象
+// Widget 小部件
+// 所有的部件分为两大类：有状态 Stateful ; 无状态 Stateless
+// build 渲染（将小部件渲染到界面）
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: MyWidget(),
+  ));
+}
+
+// 一个 Widget 就是一个类
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        'hello flutter',
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
